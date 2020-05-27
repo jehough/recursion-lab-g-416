8aff34a8479142cb6a8398df429b5303993b4aeb
Jim Hough <teachlife@outlook.com>
2020-05-27 10:43:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,45 @@
 // Code your solution here!
 
+function printString(string){
+  console.log(string[0])
+  if (string.length > 1){
+    let mySubstring = string.substring(1, string.length);
+    printString(mySubstring)}
+  else{
+    return true
+  }
+}
+
+function reverseString(myString) {
+  if (myString.length < 2) {
+    return myString;
+  } else {
+    return reverseString(myString.substring(1)) + myString[0];
+  }
+}
+
+function isPalindrome(string){
+  let l = string.length
+  if(l < 2){
+    return true
+  }
+  else if(string[0] === string[string.length-1]){
+    return isPalindrome(string.substring(1,string.length - 1))
+  }
+  else{
+    return false
+  }
+}
+
+function addUpTo(arr, ind){
+  if(ind ){
+    return arr[ind] + addUpTo(arr, ind-1)
+  }
+  else{
+    return arr[ind]
+  }
+}
+
+function maxOf(){
+
+}
